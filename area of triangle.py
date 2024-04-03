1960f3d941bab6e610d1e5eaf9e48d334d9f2850
Girija250 <151841575+Girija250@users.noreply.github.com>
2024-04-03 13:54:36 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,12 @@
+
+a = 5
+b = 6
+area=0.5*a*b
+print('The area of the triangle is = ',area)
+#using harons formula
+a=5
+b=6
+c=7
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print ("The area of triangle is :",area)
---
+++ arithmatic.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+print("addition:",a+b+c)
+print("subtraction:",a-b-c)
+print("multiplication:",a*b*c)
+print("modulous:",a%b)
+print("power:",a**b)
